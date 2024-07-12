8143dc7cf6375c509acf69109a1dfb78bfc49ac6
Srujankm12 <srujankm12@gmail.com>
2024-07-12 22:23:25 +0530

    learnt put

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -26,27 +26,33 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.addProduct(rw, r)
 		return
 	}
-
 	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+		// expect the id in the URI
 		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindStringSubmatch(r.URL.Path, -1)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
 
 		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
 			return
 		}
-		if len(g[0]) != 1 {
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
 			return
 		}
+
 		idString := g[0][1]
 		id, err := strconv.Atoi(idString)
 		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
 			return
 		}
+		p.l.Println("id", id)
 
-		p.l.Println("got id", id)
 	}
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
