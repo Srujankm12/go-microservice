b97701be3a170995bd0b1f91422af9dbda41b096
Srujankm12 <srujankm12@gmail.com>
2024-07-13 07:49:25 +0530

    update

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -5,20 +5,26 @@ import (
 	"net/http"
 	"regexp"
 	"strconv"
+
 	"yes/data"
 )
 
+// Products is a http.Handler
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
+// interface
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle the request for a list of products
 	if r.Method == http.MethodGet {
-		p.GetProducts(rw, r)
+		p.getProducts(rw, r)
 		return
 	}
 
@@ -26,6 +32,7 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.addProduct(rw, r)
 		return
 	}
+
 	if r.Method == http.MethodPut {
 		p.l.Println("PUT", r.URL.Path)
 		// expect the id in the URI
@@ -51,15 +58,24 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
 			return
 		}
-		p.l.Println("id", id)
 
+		p.updateProducts(id, rw, r)
+		return
 	}
 
+	// catch all
+	// if no method is satisfied return an error
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
 	lp := data.GetProducts()
+
+	// serialize the list to JSON
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
@@ -67,14 +83,36 @@ func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("handle Post products")
+	p.l.Println("Handle POST Product")
+
 	prod := &data.Product{}
 
 	err := prod.FromJSON(r.Body)
 	if err != nil {
 		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
-		return
 	}
 
 	data.AddProduct(prod)
+}
+
+func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
